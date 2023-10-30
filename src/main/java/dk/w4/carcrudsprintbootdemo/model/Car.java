61022d1fcfa48c29c5eb8a064cd905e99350d90b
Steffen Mogensen <feber.sm@gmail.com>
2023-10-30 06:43:10 +0100

    code clean-up

--- src/main/java/dk/w4/carcrudsprintbootdemo/model/Car.java
+++ src/main/java/dk/w4/carcrudsprintbootdemo/model/Car.java
@@ -14,28 +14,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Car{
-
-        @Id
-        String vin;
-        String make;
-        String model;
-        double mileage;
-        int year;
-        String color;
-        long createdAt;
-        long updatedAt;
-
-        /*
-    public static Car empty() {
-        return new Car("", "", "", 0, 0, "", 0, 0);
-    }
-    public Car setCreatedAt(long unixTimeStamp) {
-        return new Car(vin, make, model, mileage, year, color, unixTimeStamp, unixTimeStamp);
-    }
-
-    public Car setUpdatedAt(long unixTimeStamp) {
-        return new Car(vin, make, model, mileage, year, color, createdAt, unixTimeStamp);
-    }
-         */
+public class Car {
+    @Id
+    String vin;
+    String make;
+    String model;
+    double mileage;
+    int year;
+    String color;
+    long createdAt;
+    long updatedAt;
 }
